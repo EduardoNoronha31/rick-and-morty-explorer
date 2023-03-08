eb86b313a4adffe8ef5c2e1b2ba96a1be07ae6d5
Eduardo Noronha <eduardo.noronha@imlabs.com.br>
2023-03-08 15:09:14 -0300

    Creating the home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,25 @@
 <template>
-  <main-layout> Hello world </main-layout>
+  <main-layout>
+    <template v-slot:content>
+      <div id="home-page" class="flex column items-center">
+        <img
+          src="src/assets/rick-and-morty-text.svg"
+          class="rick-and-morty-text-image q-mt-xl animate__animated animate__bounceIn"
+        />
+
+        <img
+          src="src/assets/rick-and-morty-spaceship.svg"
+          class="rick-and-morty-spaceship q-mt-md animate__animated animate__zoomInUp animate__slow"
+        />
+      </div>
+    </template>
+  </main-layout>
 </template>
 
 <script>
-import MainLayout from "src/layouts/MainLayout.vue";
+import "animate.css";
 import { defineComponent } from "vue";
+import MainLayout from "src/layouts/MainLayout.vue";
 
 export default defineComponent({
   name: "HomePage",
@@ -12,3 +27,29 @@ export default defineComponent({
   components: { MainLayout },
 });
 </script>
+
+<style lang="scss" scoped>
+#home-page {
+  height: calc(100vh - 80px);
+
+  .rick-and-morty-text-image,
+  .rick-and-morty-spaceship {
+    max-width: 750px;
+    min-width: 350px;
+    max-height: 286px;
+    width: 100vw;
+    padding: 20px;
+  }
+}
+
+@media (max-width: 600px) {
+  #home-page {
+    justify-content: center;
+
+    .rick-and-morty-text-image,
+    .rick-and-morty-spaceship {
+      margin-top: 0;
+    }
+  }
+}
+</style>
